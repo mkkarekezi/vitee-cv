4811f036302ae633ae4b4a0d7dec2667fe9e72aa
mkkarekezi <m.kkarekezi@gmail.com>
2026-01-14 18:31:14 +0000

    commit

--- vitee-cv-frontend/app/layout.jsx
+++ vitee-cv-frontend/app/layout.jsx
@@ -22,6 +22,12 @@ const Geist = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${Geist.variable}`}>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
